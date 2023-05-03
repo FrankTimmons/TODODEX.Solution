465c587f997fe23e38024c3a1790d479fcbd6e0a
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-02 21:01:13 -0700

    Fix the ApiHelper.cs to work with a locally hosted API.

--- ToDoClient.Solution/Models/ApiHelper.cs
+++ ToDoClient.Solution/Models/ApiHelper.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
+using System;
 using RestSharp;
+
 
 namespace ToDoClient.Solution.Models
 {
@@ -7,7 +9,7 @@ namespace ToDoClient.Solution.Models
   {
     public static async Task<string> GetAll()
     {
-      RestClient client = new RestClient("https://tododexapi.azurewebsites.net/api/");
+      RestClient client = new RestClient("http://localhost:5002/api");
       RestRequest request = new RestRequest($"todos", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
@@ -15,7 +17,7 @@ namespace ToDoClient.Solution.Models
 
     public static async Task<string> Get(int id)
     {
-      RestClient client = new RestClient("https://tododexapi.azurewebsites.net/api/");
+      RestClient client = new RestClient("http://localhost:5002/api");
       RestRequest request = new RestRequest($"todos/{id}", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
@@ -23,7 +25,7 @@ namespace ToDoClient.Solution.Models
 
     public static async Task Post(string newToDo)
     {
-      RestClient client = new RestClient("https://tododexapi.azurewebsites.net/api/");
+      RestClient client = new RestClient("http://localhost:5002/api");
       RestRequest request = new RestRequest($"todos", Method.POST);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newToDo);
@@ -32,7 +34,7 @@ namespace ToDoClient.Solution.Models
 
     public static async Task Put(int id, string newToDo)
     {
-      RestClient client = new RestClient("https://tododexapi.azurewebsites.net/api/");
+      RestClient client = new RestClient("http://localhost:5002/api");
       RestRequest request = new RestRequest($"todos/{id}", Method.PUT);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newToDo);
@@ -41,7 +43,7 @@ namespace ToDoClient.Solution.Models
 
     public static async Task Delete(int id)
     {
-      RestClient client = new RestClient("https://tododexapi.azurewebsites.net/api/");
+      RestClient client = new RestClient("http://localhost:5002/api");
       RestRequest request = new RestRequest($"todos/{id}", Method.DELETE);
       request.AddHeader("Content-Type", "application/json");
       var response = await client.ExecuteTaskAsync(request);
